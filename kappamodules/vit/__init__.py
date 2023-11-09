92b6788cdf0cdd9069979df6da6ef6aae07bc3f0
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-11-09 23:08:10 +0100

    a

---
+++ kappamodules/vit/__init__.py
@@ -0,0 +1,2 @@
+from .vit_mlp import VitMlp
+from .vit_block import VitBlock
---
+++ test_unit/vit/test_vit_mlp.py
@@ -0,0 +1,20 @@
+import unittest
+
+import torch
+from timm.layers import Mlp
+
+from kappamodules.vit import VitMlp
+
+
+class TestVitMlp(unittest.TestCase):
+    def test_equal_to_timm(self):
+        seed = 89453
+        torch.manual_seed(seed)
+        mlp0 = Mlp(in_features=4, hidden_features=12)
+        torch.manual_seed(seed)
+        mlp1 = VitMlp(in_dim=4, hidden_dim=12)
+        torch.manual_seed(seed)
+        x = torch.randn(3, 4)
+        y0 = mlp0(x)
+        y1 = mlp1(x)
+        self.assertTrue(torch.all(y0 == y1))
