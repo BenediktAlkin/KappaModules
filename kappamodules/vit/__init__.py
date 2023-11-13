6409dc75cd4137ddc7f75e359b6ac1c85a3ebb24
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-11-13 15:56:07 +0100

    Update __init__.py

--- kappamodules/vit/__init__.py
+++ kappamodules/vit/__init__.py
@@ -1,8 +1,8 @@
 from .vit_batchnorm import VitBatchNorm1d, VitBatchNorm2d, VitBatchNorm3d
 from .vit_block import VitBlock
 from .vit_blocks import VitBlocks
+from .vit_class_tokens import VitClassTokens
 from .vit_mlp import VitMlp
 from .vit_patch_embed import VitPatchEmbed
+from .vit_pos_embed import VitPosEmbed1d, VitPosEmbed2d, VitPosEmbed3d, VitPosEmbedNd
 from .vit_seperate_norm import VitSeperateNorm
-from .vit_pos_embed import VitPosEmbed1d, VitPosEmbed2d, VitPosEmbed3d
-from .vit_class_tokens import VitClassTokens
