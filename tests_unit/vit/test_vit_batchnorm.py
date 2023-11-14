99a29e524df6dd4cda47180665addace8e57708d
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-11-14 20:44:04 +0100

    Update test_vit_batchnorm.py

--- tests_unit/vit/test_vit_batchnorm.py
+++ tests_unit/vit/test_vit_batchnorm.py
@@ -7,7 +7,7 @@ class TestVitBatchNorm(unittest.TestCase):
     def test_0d(self):
         dim = 4
         bn = VitBatchNorm(num_features=dim, eps=0)
-        x = torch.rand(16, dim, generator=torch.Generator().manual_seed(89543))
+        x = torch.rand(10, dim, generator=torch.Generator().manual_seed(89543))
         y = bn(x)
         self.assertEqual(x.shape, y.shape)
         self.assertTrue(torch.allclose(y.mean(dim=0), torch.zeros(size=(dim,)), atol=1e-6))
@@ -16,8 +16,26 @@ class TestVitBatchNorm(unittest.TestCase):
     def test_1d(self):
         dim = 4
         bn = VitBatchNorm(num_features=dim, eps=0)
-        x = torch.rand(16, 4, dim, generator=torch.Generator().manual_seed(89543))
+        x = torch.rand(5, 4, dim, generator=torch.Generator().manual_seed(89543))
         y = bn(x)
         self.assertEqual(x.shape, y.shape)
         self.assertTrue(torch.allclose(y.mean(dim=[0, 1]), torch.zeros(size=(dim,)), atol=1e-6))
         self.assertTrue(torch.allclose(y.std(dim=[0, 1]), torch.ones(size=(dim,)), atol=1e-1))
+
+    def test_2d(self):
+        dim = 4
+        bn = VitBatchNorm(num_features=dim, eps=0)
+        x = torch.rand(5, 4, 3, dim, generator=torch.Generator().manual_seed(89543))
+        y = bn(x)
+        self.assertEqual(x.shape, y.shape)
+        self.assertTrue(torch.allclose(y.mean(dim=[0, 1, 2]), torch.zeros(size=(dim,)), atol=1e-6))
+        self.assertTrue(torch.allclose(y.std(dim=[0, 1, 2]), torch.ones(size=(dim,)), atol=1e-1))
+
+    def test_3d(self):
+        dim = 4
+        bn = VitBatchNorm(num_features=dim, eps=0)
+        x = torch.rand(5, 4, 3, 2, dim, generator=torch.Generator().manual_seed(89543))
+        y = bn(x)
+        self.assertEqual(x.shape, y.shape)
+        self.assertTrue(torch.allclose(y.mean(dim=[0, 1, 2, 3]), torch.zeros(size=(dim,)), atol=1e-6))
+        self.assertTrue(torch.allclose(y.std(dim=[0, 1, 2, 3]), torch.ones(size=(dim,)), atol=1e-1))
