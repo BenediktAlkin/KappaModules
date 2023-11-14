49bdd7fa89ab891f92b5e1c4fe7bed733a5900b0
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-11-14 20:13:05 +0100

    fix vitbatchnorm

--- kappamodules/vit/__init__.py
+++ kappamodules/vit/__init__.py
@@ -1,9 +1,9 @@
 from .dit_block import DitBlock
-from .vit_batchnorm import VitBatchNorm1d, VitBatchNorm2d, VitBatchNorm3d
+from .vit_batchnorm import VitBatchNorm
 from .vit_block import VitBlock
 from .vit_blocks import VitBlocks
 from .vit_class_tokens import VitClassTokens
 from .vit_mlp import VitMlp
 from .vit_patch_embed import VitPatchEmbed
-from .vit_pos_embed import VitPosEmbed1d, VitPosEmbed2d, VitPosEmbed3d, VitPosEmbedNd
+from .vit_pos_embed import VitPosEmbed1d, VitPosEmbed2d, VitPosEmbed3d, VitPosEmbed, VitPosEmbedNd
 from .vit_seperate_norm import VitSeperateNorm
