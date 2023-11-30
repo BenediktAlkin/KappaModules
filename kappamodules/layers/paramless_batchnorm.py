c5f012868a88b99886a864a9af7d3d5e04f9fa42
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-11-30 16:27:05 +0100

    Update paramless_batchnorm.py

--- kappamodules/layers/paramless_batchnorm.py
+++ kappamodules/layers/paramless_batchnorm.py
@@ -1,12 +1,13 @@
 from torch import nn
 
 
-class ParamlessBatchNorm1d(nn.BatchNorm1d):
+class ParamlessBatchNorm1d(nn.Module):
     """ non-affine BatchNorm1d layer that doesn't need a dimension but also can't be used in eval mode """
 
     def __init__(self):
-        super().__init__(num_features=None, affine=False, track_running_stats=False)
+        super().__init__()
+        self.norm = nn.BatchNorm1d(num_features=None, affine=False, track_running_stats=False)
 
     def forward(self, x):
         assert self.training
-        return super().forward(x)
+        return self.norm(x)
