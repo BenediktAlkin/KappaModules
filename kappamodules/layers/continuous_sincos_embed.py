4866cf2e89306d3dcd08fe37a29fa411edba3a80
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-12-05 22:11:38 +0100

    a

--- kappamodules/layers/continuous_sincos_embed.py
+++ kappamodules/layers/continuous_sincos_embed.py
@@ -4,24 +4,22 @@ from torch import nn
 
 
 class ContinuousSincosEmbed(nn.Module):
-    def __init__(self, dim, ndim, max_wavelength: int = 10000, dtype=torch.double, output_dtype=torch.float32):
+    def __init__(self, dim, ndim, max_wavelength: int = 10000):
         super().__init__()
         assert dim % ndim == 0
         self.dim = dim
         self.ndim = ndim
         self.max_wavelength = max_wavelength
-        self.dtype = dtype
-        self.output_dtype = output_dtype
         dim_div_ndim = dim // ndim
         self.register_buffer(
             "omega",
-            1. / max_wavelength ** (torch.arange(0, dim_div_ndim, 2, dtype=dtype) / dim_div_ndim),
+            1. / max_wavelength ** (torch.arange(0, dim_div_ndim, 2, dtype=torch.float32) / dim_div_ndim),
         )
 
     def forward(self, coords):
         ndim = coords.shape[-1]
         assert self.ndim == ndim
-        out = coords.unsqueeze(-1).to(self.dtype) @ self.omega.unsqueeze(0)
+        out = coords.unsqueeze(-1) @ self.omega.unsqueeze(0).to(coords.dtype)
         emb = torch.concat([torch.sin(out), torch.cos(out)], dim=-1)
         if coords.ndim == 3:
             emb = einops.rearrange(emb, "bs num_points ndim dim -> bs num_points (ndim dim)")
@@ -29,4 +27,4 @@ class ContinuousSincosEmbed(nn.Module):
             emb = einops.rearrange(emb, "bs_times_num_points ndim dim -> bs_times_num_points (ndim dim)")
         else:
             raise NotImplementedError
-        return emb.to(self.output_dtype)
+        return emb
--- tests_unit/layers/test_continuous_sincos_embed.py
+++ tests_unit/layers/test_continuous_sincos_embed.py
@@ -18,3 +18,10 @@ class TestContinuousSincosEmbed(unittest.TestCase):
         embed = ContinuousSincosEmbed(dim=dim, ndim=ndim)
         sparse_result = embed(sparse_coords)
         self.assertEqual((batch_size * num_points, dim), sparse_result.shape)
+
+    def test_dtype(self):
+        ndim = 2
+        coords = torch.rand(3, 32, ndim, generator=torch.Generator().manual_seed(8943))
+        embed = ContinuousSincosEmbed(dim=16, ndim=ndim)
+        result = embed(coords.double())
+        self.assertEqual(torch.double, result.dtype)
