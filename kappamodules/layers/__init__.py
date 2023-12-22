4b8f056e5dd0f5234c191551f590d8533fcc355b
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-12-22 13:18:59 +0100

    RegularGridSincosEmbed

--- kappamodules/layers/__init__.py
+++ kappamodules/layers/__init__.py
@@ -5,3 +5,4 @@ from .normalize import Normalize
 from .paramless_batchnorm import ParamlessBatchNorm1d
 from .residual import Residual
 from .weight_norm_linear import WeightNormLinear
+from .regular_grid_sincos_embed import RegularGridSincosEmbed
---
+++ kappamodules/layers/regular_grid_sincos_embed.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+
+from kappamodules.functional.pos_embed import get_sincos_pos_embed_from_seqlens, interpolate_sincos
+
+
+class RegularGridSincosEmbed(nn.Module):
+    def __init__(self, seqlens, dim: int, is_learnable: bool = False, allow_interpolation: bool = True):
+        super().__init__()
+        self.seqlens = seqlens
+        self.dim = dim
+        self.is_learnable = is_learnable
+        self.allow_interpolation = allow_interpolation
+        if is_learnable:
+            self.embed = nn.Parameter(torch.zeros(1, *seqlens, dim))
+        else:
+            self.register_buffer("embed", get_sincos_pos_embed_from_seqlens(seqlens=seqlens, dim=dim).unsqueeze(0))
+        self.reset_parameters()
+
+    @property
+    def _expected_x_ndim(self):
+        return len(self.seqlens) + 2
+
+    def reset_parameters(self):
+        if self.is_learnable:
+            nn.init.trunc_normal_(self.embed, std=.02)
+
+    def forward(self, x):
+        assert x.ndim == self._expected_x_ndim
+        if x.shape[1:] != self.embed.shape[1:]:
+            assert self.allow_interpolation
+            embed = interpolate_sincos(embed=self.embed, seqlens=x.shape[1:-1])
+        else:
+            embed = self.embed
+        return embed
+
