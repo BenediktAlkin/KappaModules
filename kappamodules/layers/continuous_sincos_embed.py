be29750d8e787fbdd1df3d880452c36980c95926
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-12-30 22:41:23 +0100

    fix posembed

--- kappamodules/layers/continuous_sincos_embed.py
+++ kappamodules/layers/continuous_sincos_embed.py
@@ -4,22 +4,27 @@ from torch import nn
 
 
 class ContinuousSincosEmbed(nn.Module):
-    def __init__(self, dim, ndim, max_wavelength: int = 10000):
+    def __init__(self, dim, ndim, max_wavelength: int = 10000, dtype=torch.float32):
         super().__init__()
-        self.padding = dim % ndim
-        self.dim = dim - self.padding
+        self.dim = dim
         self.ndim = ndim
+        # if dim is not cleanly divisible -> cut away trailing dimensions
+        self.ndim_padding = dim % ndim
+        dim_per_ndim = (dim - self.ndim_padding) // ndim
+        self.sincos_padding = dim_per_ndim % 2
         self.max_wavelength = max_wavelength
-        dim_div_ndim = self.dim // ndim
+        self.padding = self.ndim_padding + self.sincos_padding * 2
+        effective_dim_per_wave = (self.dim - self.padding) // ndim
         self.register_buffer(
             "omega",
-            1. / max_wavelength ** (torch.arange(0, dim_div_ndim, 2, dtype=torch.float32) / dim_div_ndim),
+            1. / max_wavelength ** (torch.arange(0, effective_dim_per_wave, 2, dtype=dtype) / effective_dim_per_wave),
         )
 
     def forward(self, coords):
+        out_dtype = coords.dtype
         ndim = coords.shape[-1]
         assert self.ndim == ndim
-        out = coords.unsqueeze(-1) @ self.omega.unsqueeze(0).to(coords.dtype)
+        out = coords.unsqueeze(-1).to(self.omega.dtype) @ self.omega.unsqueeze(0)
         emb = torch.concat([torch.sin(out), torch.cos(out)], dim=-1)
         if coords.ndim == 3:
             emb = einops.rearrange(emb, "bs num_points ndim dim -> bs num_points (ndim dim)")
@@ -27,4 +32,8 @@ class ContinuousSincosEmbed(nn.Module):
             emb = einops.rearrange(emb, "bs_times_num_points ndim dim -> bs_times_num_points (ndim dim)")
         else:
             raise NotImplementedError
+        emb = emb.to(out_dtype)
+        if self.padding > 0:
+            padding = torch.zeros(*emb.shape[:-1], self.padding, device=emb.device, dtype=emb.dtype)
+            emb = torch.concat([emb, padding], dim=-1)
         return emb
--- tests_unit/layers/test_continuous_sincos_embed.py
+++ tests_unit/layers/test_continuous_sincos_embed.py
@@ -1,6 +1,9 @@
 import unittest
+
+import einops
 import torch
 from kappamodules.layers import ContinuousSincosEmbed
+from kappamodules.functional.pos_embed import get_sincos_pos_embed_from_seqlens
 
 class TestContinuousSincosEmbed(unittest.TestCase):
     def test_shape(self):
@@ -26,9 +29,51 @@ class TestContinuousSincosEmbed(unittest.TestCase):
         result = embed(coords.double())
         self.assertEqual(torch.double, result.dtype)
 
+    def test_equals_regulargrid_square2d(self):
+        seqlens = (4, 4)
+        dim = 16
+        expected = get_sincos_pos_embed_from_seqlens(seqlens=seqlens, dim=dim, indexing="ij")
+        grids = torch.meshgrid(*[torch.arange(seqlen, dtype=torch.float32) for seqlen in seqlens], indexing="ij")
+        coords = einops.rearrange(torch.stack(grids), "ndim ... -> (...) ndim")
+        actual = ContinuousSincosEmbed(dim=dim, ndim=len(seqlens), dtype=torch.double)(coords).reshape(*expected.shape)
+        self.assertTrue(torch.all(expected == actual))
+
+    def test_equals_regulargrid_rect2d(self):
+        seqlens = (3, 4)
+        dim = 16
+        expected = get_sincos_pos_embed_from_seqlens(seqlens=seqlens, dim=dim, indexing="ij")
+        coords = torch.meshgrid(*[torch.arange(seqlen, dtype=torch.float32) for seqlen in seqlens], indexing="ij")
+        coords = einops.rearrange(torch.stack(coords), "ndim ... -> (...) ndim")
+        actual = ContinuousSincosEmbed(dim=dim, ndim=len(seqlens), dtype=torch.double)(coords).reshape(*expected.shape)
+        self.assertTrue(torch.all(expected == actual))
+
+    def test_equals_regulargrid_rect3d(self):
+        seqlens = (3, 4, 5)
+        dim = 18
+        expected = get_sincos_pos_embed_from_seqlens(seqlens=seqlens, dim=dim, indexing="ij")
+        coords = torch.meshgrid(*[torch.arange(seqlen, dtype=torch.float32) for seqlen in seqlens], indexing="ij")
+        coords = einops.rearrange(torch.stack(coords), "ndim ... -> (...) ndim")
+        actual = ContinuousSincosEmbed(dim=dim, ndim=len(seqlens), dtype=torch.double)(coords).reshape(*expected.shape)
+        self.assertTrue(torch.all(expected == actual))
+
+    def test_padding_equals_regulargridpadding(self):
+        seqlens = (3, 4)
+        dim = 15
+        expected = get_sincos_pos_embed_from_seqlens(seqlens=seqlens, dim=dim, indexing="ij")
+        coords = torch.meshgrid(*[torch.arange(seqlen, dtype=torch.float32) for seqlen in seqlens], indexing="ij")
+        coords = einops.rearrange(torch.stack(coords), "ndim ... -> (...) ndim")
+        actual = ContinuousSincosEmbed(dim=dim, ndim=len(seqlens), dtype=torch.double)(coords).reshape(*expected.shape)
+        # continuous has all padding as last dimensions, static embed has the sincos padding in the middle
+        # continuous padded dims are: 12, 13, 14
+        # fixed padded dims are: 6, 13, 14
+        self.assertTrue(torch.all(expected[:, :, :5] == actual[:, :, :5]))
+        self.assertTrue(torch.all(expected[:, :, 6] == actual[:, :, 12]))
+        self.assertTrue(torch.all(expected[:, :, 7:13] == actual[:, :, 6:12]))
+        self.assertTrue(torch.all(expected[:, :, 13:] == actual[:, :, 13:]))
+        self.assertTrue(torch.all(actual[:, :, 13:] == 0))
+
     def test_padding(self):
         ndim = 2
         coords = torch.rand(3, 32, ndim, generator=torch.Generator().manual_seed(8943))
-        embed = ContinuousSincosEmbed(dim=15, ndim=ndim)
-        result = embed(coords)
-        self.assertEqual(torch.double, result.dtype)
+        result = ContinuousSincosEmbed(dim=15, ndim=ndim)(coords)
+        self.assertEqual((3, 32, 15), result.shape)
