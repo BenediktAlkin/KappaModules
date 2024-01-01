9bf6ed3f6f68b53f28c4ca134e41718978cd31d4
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-01-01 14:30:29 +0100

    LinearProjection

--- kappamodules/layers/__init__.py
+++ kappamodules/layers/__init__.py
@@ -1,6 +1,7 @@
 from .continuous_sincos_embed import ContinuousSincosEmbed
 from .drop_path import DropPath
 from .identity import Identity
+from .linear_projection import LinearProjection
 from .normalize import Normalize
 from .paramless_batchnorm import ParamlessBatchNorm1d
 from .regular_grid_sincos_embed import RegularGridSincosEmbed
---
+++ kappamodules/layers/linear_projection.py
@@ -0,0 +1,24 @@
+from torch import nn
+from kappamodules.init import init_xavier_uniform_zero_bias
+from torch import nn
+
+from kappamodules.init import init_xavier_uniform_zero_bias
+
+
+class LinearProjection(nn.Modules):
+    def __init__(self, input_dim, output_dim, bias=True, init_weights="xavier_uniform"):
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.bias = bias
+        self.init_weights = init_weights
+        self.proj = nn.Linear(input_dim, output_dim, bias=bias)
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+        if self.init_weights == "torch":
+            pass
+        elif self.init_weights == "nonaffine":
+            init_xavier_uniform_zero_bias(self.proj)
+        else:
+            raise NotImplementedError
