eea6a15d3842f5ce5b1673e4d3f797376db6db72
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-01-03 11:01:33 +0100

    Update __init__.py

--- kappamodules/transformer/__init__.py
+++ kappamodules/transformer/__init__.py
@@ -1,5 +1,5 @@
+from .mlp import Mlp
 from .perceiver_block import PerceiverBlock
 from .perceiver_pooling_block import PerceiverPoolingBlock
 from .postnorm_block import PostnormBlock
-from .mlp import Mlp
 from .prenorm_block import PrenormBlock
