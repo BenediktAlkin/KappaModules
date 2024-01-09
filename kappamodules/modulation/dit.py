f5b16e90aa6cdf0958d7e359eab1db737455415d
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-01-09 20:43:04 +0100

    Update dit.py

--- kappamodules/modulation/dit.py
+++ kappamodules/modulation/dit.py
@@ -4,18 +4,19 @@ from torch import nn
 
 
 class Dit(nn.Module):
-    def __init__(self, cond_dim, out_dim, init_weights="xavier_uniform"):
+    def __init__(self, cond_dim, out_dim, num_outputs=6, init_weights="xavier_uniform"):
         super().__init__()
         self.in_dim = cond_dim
         self.out_dim = out_dim
-        self.modulation = nn.Linear(cond_dim, 6 * out_dim)
+        self.num_outputs = num_outputs
+        self.modulation = nn.Linear(cond_dim, num_outputs * out_dim)
         self.init_weights = init_weights
         self.reset_parameters()
 
     def reset_parameters(self):
         fan_in = self.modulation.weight.shape[1]
-        assert self.modulation.weight.shape[0] % 6 == 0
-        fan_out = self.modulation.weight.shape[0] // 6
+        assert self.modulation.weight.shape[0] % self.num_outputs == 0
+        fan_out = self.modulation.weight.shape[0] // self.num_outputs
         if self.init_weights == "torch":
             pass
         elif self.init_weights == "xavier_uniform":
@@ -26,5 +27,6 @@ class Dit(nn.Module):
             raise NotImplementedError
 
     def forward(self, cond):
-        scale1, shift1, gate1, scale2, shift2, gate2 = self.modulation(cond).chunk(6, dim=1)
-        return scale1, shift1, gate1, scale2, shift2, gate2
+        # exmple: transformer block
+        # scale1, shift1, gate1, scale2, shift2, gate2 = self.modulation(cond).chunk(6, dim=1)
+        return self.modulation(cond).chunk(self.num_outputs, dim=1)
