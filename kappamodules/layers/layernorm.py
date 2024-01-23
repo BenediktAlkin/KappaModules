afc9e6b053c492d0f4ff7df0f3d6f96b5a30f7d1
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-01-23 01:29:14 +0100

    LayerNorm2d

---
+++ kappamodules/layers/layernorm.py
@@ -0,0 +1,26 @@
+import einops
+from torch import nn
+
+
+class LayerNorm2d(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.layer = nn.LayerNorm(*args, **kwargs)
+
+    def forward(self, x):
+        x = einops.rearrange(x, "batch_size dim height width -> batch_size height width dim")
+        x = self.layer(x)
+        x = einops.rearrange(x, "batch_size height width dim -> batch_size dim height width")
+        return x
+
+
+class LayerNorm3d(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.layer = nn.LayerNorm(*args, **kwargs)
+
+    def forward(self, x):
+        x = einops.rearrange(x, "batch_size dim height width depth -> batch_size height width depth dim")
+        x = self.layer(x)
+        x = einops.rearrange(x, "batch_size height width depth dim -> batch_size dim height width depth")
+        return x
