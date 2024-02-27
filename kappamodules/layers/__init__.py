7b65ff51134c655da22fdbdadc37d66dfffc0f24
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-02-27 18:43:44 +0100

    learned batchnorm

--- kappamodules/layers/__init__.py
+++ kappamodules/layers/__init__.py
@@ -3,6 +3,7 @@ from .drop_path import DropPath
 from .identity import Identity
 from .ignore_args_and_kwargs_wrapper import IgnoreArgsAndKwargsWrapper
 from .layernorm import LayerNorm1d, LayerNorm2d, LayerNorm3d
+from .learned_batchnorm import LearnedBatchNorm, LearnedBatchNorm1d, LearnedBatchNorm2d, LearnedBatchNorm3d
 from .linear_projection import LinearProjection
 from .normalize import Normalize
 from .paramless_batchnorm import ParamlessBatchNorm1d
---
+++ kappamodules/layers/learned_batchnorm.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+
+class LearnedBatchNorm(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+        self.mean = nn.Parameter(torch.zeros(*self._shape()))
+        self.logvar = nn.Parameter(torch.zeros(*self._shape()))
+        self.weight = nn.Parameters(torch.ones(*self._shape()))
+        self.bias = nn.Parameters(torch.zeros(*self._shape()))
+
+    def _shape(self):
+        return 1, self.dim
+
+    def forward(self, x):
+        return F.batch_norm(
+            input=x,
+            running_mean=self.mean,
+            running_var=self.logvar.exp(),
+            weight=self.weight,
+            bias=self.bias,
+            # avoid updating mean/var
+            training=False,
+        )
+
+
+class LearnedBatchNorm1d(LearnedBatchNorm):
+    def _shape(self):
+        return 1, self.dim, 1
+
+
+class LearnedBatchNorm2d(LearnedBatchNorm):
+    def _shape(self):
+        return 1, self.dim, 1, 1
+
+
+class LearnedBatchNorm3d(LearnedBatchNorm):
+    def _shape(self):
+        return 1, self.dim, 1, 1, 1
