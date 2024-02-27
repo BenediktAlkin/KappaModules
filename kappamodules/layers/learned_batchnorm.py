41a7bb9c20a15246c6ea49959133268a6ebf2be3
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-02-27 18:47:55 +0100

    Update learned_batchnorm.py

--- kappamodules/layers/learned_batchnorm.py
+++ kappamodules/layers/learned_batchnorm.py
@@ -4,13 +4,18 @@ from torch import nn
 
 
 class LearnedBatchNorm(nn.Module):
-    def __init__(self, dim):
+    def __init__(self, dim, affine=True):
         super().__init__()
         self.dim = dim
+        self.affine = affine
         self.mean = nn.Parameter(torch.zeros(*self._shape()))
         self.logvar = nn.Parameter(torch.zeros(*self._shape()))
-        self.weight = nn.Parameters(torch.ones(*self._shape()))
-        self.bias = nn.Parameters(torch.zeros(*self._shape()))
+        if affine:
+            self.weight = nn.Parameters(torch.ones(*self._shape()))
+            self.bias = nn.Parameters(torch.zeros(*self._shape()))
+        else:
+            self.weight = None
+            self.bias = None
 
     def _shape(self):
         return 1, self.dim
