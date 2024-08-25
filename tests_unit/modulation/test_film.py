1904ef4dd76487b9bff77be0426bbcb38caf5257
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-08-25 13:22:38 +0200

    film channel_last

---
+++ tests_unit/modulation/test_film.py
@@ -0,0 +1,12 @@
+import unittest
+from kappamodules.modulation import Film
+import torch
+
+class TestFilm(unittest.TestCase):
+    def test_channel_last(self):
+        film = Film(dim_cond=5, dim_out=4, channel_first=False)
+        cond = torch.randn(2, 5, generator=torch.Generator().manual_seed(0))
+        x = torch.randn(2, 3, 4, generator=torch.Generator().manual_seed(0))
+        x = film(x, cond)
+        self.assertEqual((2, 3, 4), x.shape)
+        self.assertTrue(torch.all(x[0] != x[1]))
