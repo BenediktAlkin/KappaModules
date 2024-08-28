edcf4bb4761ebbbc0ac74d01fc4db56ffa79c64d
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-08-28 11:18:02 +0200

    Update layer_scale.py

--- kappamodules/layers/layer_scale.py
+++ kappamodules/layers/layer_scale.py
@@ -5,7 +5,12 @@ from torch import nn
 class LayerScale(nn.Module):
     def __init__(self, dim: int, init_scale: float = 1e-5):
         super().__init__()
-        self.gamma = nn.Parameter(torch.full(size=(dim,), fill_value=init_scale))
+        if init_scale is None:
+            self.gamma = None
+        else:
+            self.gamma = nn.Parameter(torch.full(size=(dim,), fill_value=init_scale))
 
     def forward(self, x):
+        if self.gamma is None:
+            return x
         return x * self.gamma
