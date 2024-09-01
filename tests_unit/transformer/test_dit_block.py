24d20ed824019d7bac3612ab15a75045b8a16033
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-09-01 10:59:49 +0200

    Update test_dit_block.py

--- tests_unit/transformer/test_dit_block.py
+++ tests_unit/transformer/test_dit_block.py
@@ -25,3 +25,14 @@ class TestDitBlock(unittest.TestCase):
         y = block(x, cond=cond)
         self.assertEqual(x.shape, y.shape)
         self.assertTrue(torch.isclose(torch.tensor(-28.474021911621094), y.sum()))
+
+    def test_gate0(self):
+        torch.manual_seed(9845)
+        dim = 8
+        seqlen = 5
+        block = DitBlock(dim=dim, num_heads=2, cond_dim=dim, init_gate_zero=True)
+        x = torch.randn(4, seqlen, dim)
+        cond = torch.randn(4, dim)
+        y = block(x, cond=cond)
+        self.assertEqual(x.shape, y.shape)
+        self.assertTrue(torch.all(x == y))
