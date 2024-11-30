bc7ee695592c967d82c13a67d48f39c4e434f63b
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2024-11-30 12:21:36 -0500

    test attention

--- kappamodules/functional/attention.py
+++ kappamodules/functional/attention.py
@@ -1,3 +1,4 @@
+import einops
 import torch
 import torch.nn.functional as F
 
@@ -26,18 +27,33 @@ def scaled_dot_product_attention(
         torch.backends.cuda.enable_flash_sdp(True)
         torch.backends.cuda.enable_math_sdp(True)
         return F.scaled_dot_product_attention(**sdpa_kwargs)
+    if backend == "flash":
+        torch.backends.cuda.enable_mem_efficient_sdp(False)
+        torch.backends.cuda.enable_flash_sdp(True)
+        torch.backends.cuda.enable_math_sdp(False)
+        return F.scaled_dot_product_attention(**sdpa_kwargs)
     if backend == "math":
         torch.backends.cuda.enable_mem_efficient_sdp(False)
         torch.backends.cuda.enable_flash_sdp(False)
         torch.backends.cuda.enable_math_sdp(True)
         return F.scaled_dot_product_attention(**sdpa_kwargs)
     if backend in ["vanilla", "flops"]:
-        assert not is_causal
         assert len(kwargs) == 0
-        batch_size, _, _, head_dim = query.shape
+        batch_size, _, seqlen_q, head_dim = query.shape
+        if is_causal:
+            assert attn_mask is None
+            assert query.shape == key.shape
+            attn_mask = torch.tril(torch.ones((seqlen_q, seqlen_q), dtype=torch.bool, device=query.device))
+            attn_mask = einops.rearrange(attn_mask, "seqlen1 seqlen2 -> 1 1 seqlen1 seqlen2")
         scale = head_dim ** -0.5
-        qquery = query * scale
-        attn = qquery @ key.transpose(-2, -1)
+        scaled_query = query * scale
+        attn = scaled_query @ key.transpose(-2, -1)
+        if attn_mask is not None:
+            assert attn_mask.ndim == attn.ndim
+            if attn_mask.dtype == torch.bool:
+                attn[~attn_mask.expand(*attn.shape)] = float("-inf")
+            else:
+                attn = attn + attn_mask
         attn = attn.softmax(dim=-1)
         attn = F.dropout(attn, p=dropout_p)
         x = attn @ value
---
+++ tests_unit/functional/test_attention.py
@@ -0,0 +1,68 @@
+import unittest
+
+import torch
+
+from kappamodules.functional.attention import scaled_dot_product_attention
+
+class TestAttention(unittest.TestCase):
+    def _run_test(self, **kwargs):
+        results = []
+        for backend in ["flash", "math", "vanilla"]:
+            results.append(scaled_dot_product_attention(**kwargs, backend=backend))
+        assert len(results) == 3
+        self.assertTrue(torch.allclose(results[0], results[1]))
+        self.assertTrue(torch.allclose(results[0], results[2]))
+
+    def test_sdpa_selfattn_nomask(self):
+        batch_size = 2
+        seqlen = 8
+        num_heads = 3
+        head_dim = 4
+        q = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(0))
+        k = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(1))
+        v = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(2))
+        self._run_test(query=q, key=k, value=v)
+
+    def test_sdpa_selfattn_boolmask(self):
+        batch_size = 2
+        seqlen = 8
+        num_heads = 3
+        head_dim = 4
+        q = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(0))
+        k = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(1))
+        v = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(2))
+        attn_mask = torch.randn(batch_size, 1, seqlen, seqlen, generator=torch.Generator().manual_seed(4)) < 0
+        self._run_test(query=q, key=k, value=v, attn_mask=attn_mask)
+
+    def test_sdpa_selfattn_floatmask(self):
+        batch_size = 2
+        seqlen = 8
+        num_heads = 3
+        head_dim = 4
+        q = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(0))
+        k = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(1))
+        v = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(2))
+        attn_mask = torch.randn(batch_size, 1, seqlen, seqlen, generator=torch.Generator().manual_seed(4))
+        self._run_test(query=q, key=k, value=v, attn_mask=attn_mask)
+
+    def test_sdpa_selfattn_causal(self):
+        batch_size = 2
+        seqlen = 8
+        num_heads = 3
+        head_dim = 4
+        q = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(0))
+        k = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(1))
+        v = torch.randn(batch_size, num_heads, seqlen, head_dim, generator=torch.Generator().manual_seed(2))
+        self._run_test(query=q, key=k, value=v, is_causal=True)
+
+    def test_sdpa_crossattn_floatmask(self):
+        batch_size = 2
+        seqlen_q = 8
+        seqlen_kv = 7
+        num_heads = 3
+        head_dim = 4
+        q = torch.randn(batch_size, num_heads, seqlen_q, head_dim, generator=torch.Generator().manual_seed(0))
+        k = torch.randn(batch_size, num_heads, seqlen_kv, head_dim, generator=torch.Generator().manual_seed(1))
+        v = torch.randn(batch_size, num_heads, seqlen_kv, head_dim, generator=torch.Generator().manual_seed(2))
+        attn_mask = torch.randn(batch_size, 1, seqlen_q, seqlen_kv, generator=torch.Generator().manual_seed(4))
+        self._run_test(query=q, key=k, value=v, attn_mask=attn_mask)
